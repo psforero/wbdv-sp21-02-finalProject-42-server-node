383f2b28db82b6a1dc2256ddf668b7b1bf76c573
kellysantana <idekellysantana@ymail.com>
2021-04-07 20:24:15 -0400

    first Commit

---
+++ services/quizzes-service.js
@@ -0,0 +1,19 @@
+const quizzes = require("./quizes.json")
+
+const createQuiz = () => {}
+const findAllQuizzes = () => {
+    return quizzes
+}
+const findQuizById = (qid) => {
+    return quizzes.find((quiz) => {
+        return (quiz._id === qid)
+    })
+}
+const updateQuiz = () => {}
+const deleteQuiz = () => {}
+
+module.exports = {
+    createQuiz,
+    findAllQuizzes, findQuizById,
+    updateQuiz, deleteQuiz
+}
